cce7cee4a3ec1f9bc9345ecb0752fd8896bc8cd5
unknown <karuppan@lab3-29.users.coe.utah.edu>
2014-09-15 15:23:22 -0600

    GLFW and Eigen initial setup.

---
+++ GfxFramework/src/Main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <GLFW\glfw3.h>
+#include <Eigen\Dense>
+
+
+using namespace Eigen;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	MatrixXd m = MatrixXd::Random(3,3);
+  m = (m + MatrixXd::Constant(3,3,1.2)) * 50;
+  cout << "m =" << endl << m << endl;
+  VectorXd v(3);
+  v << 1, 2, 3;
+  cout << "m * v =" << endl << m * v << endl;
+	 GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
